89b2a255bd9cd031b1dd0bb044725f4f7dad3aa3
Ferdis <fmuriyesu@gmail.com>
2022-10-05 16:02:25 -0400

    two sum, the solution works

--- sept_2022/designpattern/SingletonLogger.java
+++ sept_2022/designpattern/SingletonLogger.java
@@ -3,6 +3,10 @@ package sept_2022.designpattern;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class SingletonLogger {
 
@@ -14,9 +18,16 @@ public class SingletonLogger {
     private SingletonLogger() {
         try {
             FileWriter fw = new FileWriter(logFile);
+            Logger logger1 = Logger.getLogger(SingletonLogger.class.getName());
             writer = new PrintWriter(fw, true);
             writer.println("Logging file for the bank operations");
             writer.println("=====================================");
+
+            FileHandler handler = new FileHandler(logFile);
+            handler.setFormatter(new SimpleFormatter());
+            logger1.addHandler(handler);
+            logger1.setLevel(Level.FINEST);
+            logger1.finest("trying this");
         } catch (IOException e) {}
     }
 
---
+++ sept_2022/someAlgorithms/TwoSum.java
@@ -0,0 +1,32 @@
+package sept_2022.someAlgorithms;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(twoSum(new int[]{2, 4, 5, 4, 7}, 7)));
+    }
+
+    public static int[] twoSum(int [] arr, int sum){
+        Map<Integer,Integer> map = new HashMap<>();
+        int [] result = new int[2];
+
+        for(int i = 0; i < arr.length; i++) {
+
+            map.put(i, arr[i]);
+
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if(map.containsValue(sum - map.get(i))) {
+                result[0] = sum - map.get(i);
+                result[1] = arr[i];
+            }
+
+        }
+        return result;
+    }
+}
