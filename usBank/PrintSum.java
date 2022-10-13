d772e0a8ff1a6df4a9b3d13693b2f50b03aab449
Ferdis <fmuriyesu@gmail.com>
2022-10-13 16:22:54 -0400

    US_Bank

---
+++ usBank/PrintSum.java
@@ -0,0 +1,41 @@
+package usBank;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class PrintSum {
+
+    // print the sum of all even numbers below 1000
+    public static int printSum (int num) {
+
+        int sum = 0;
+//        for (int i = 0; i <= num; i++) {
+//            if(i%2 == 0){
+//                sum +=i;
+//            }
+//        }
+
+       sum = IntStream.rangeClosed(0,1000).filter(n -> n % 2 == 0).sum();
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(printSum(1000));
+
+        //given two arrays and merge them numbers without duplications and in ascending order? Given arrays are: {1,7,3,4,8}, {9,2,3,1,6}
+        int[] arr1 = new int[] {1,7,3,4,8};
+        int[] arr2 = new int[] {9,2,3,1,6};
+
+        int [] mergedArray = IntStream.concat(IntStream.of(arr1),IntStream.of(arr2))
+                .distinct()
+                .sorted()
+                .toArray();
+
+        System.out.println(Arrays.toString(mergedArray));
+    }
+
+
+}
